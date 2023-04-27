9c06399f983068edcf61a5643be16a2a74705004
Samratabhi <mech.abhi22@gmail.com>
2023-04-27 15:19:06 +0530

    enum and struct committed

--- CsharpProject/ENUM And Struct/EnumClas.cs
+++ CsharpProject/ENUM And Struct/EnumClas.cs
@@ -1,30 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CsharpProject.ENUM_And_Struct
 {
     public class EnumClas
+
     {
-        enum Level
+        public enum level
         {
-            //Venus = 0,//by default its value is 0
-            Venus = 5, //if we change chane it to 1 or 5 it will change and move so on 
-           Mercury,
-           Earth,
-           Mars,
-           Jupiter,
-           Saturn,
-           Neptune,
-           Pluto
+            Venus,
+            Mercury,
+            Earth,
+            Mars,
+            Jupiter,
+            Saturn,
+            Neptune,
+            Pluto
+        }
+
+        public static void Mainnn()
+        {
+
+            EnumClas enumClas = new EnumClas();
+            Console.WriteLine(EnumClas.level.Mars);
+           
+
 
         }
-       public  static void Main(string[] args)
+
+    }
+    public class EnumCls 
+    {
+        public static void Mattin()
         {
-            Level myVar = Level.Earth;
-            Console.WriteLine(myVar);
+
+            //EnumClas enumClas = new EnumClas();
+            Console.WriteLine(EnumClas.level.Mars);
+
+
+
         }
     }
+
 }
+
---
+++ CsharpProject/ENUM And Struct/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpProject.ENUM_And_Struct
+{//C# Struct Example
+    public class Rectangle
+    {
+
+        public int width, height;
+
+    }
+    public class TestStructs
+    {
+        public static void cbain()
+        {
+            Rectangle r = new Rectangle();
+            r.width = 4;
+            r.height = 5;
+            Console.WriteLine("Area of Rectangle is: " + (r.width * r.height));
+        }
+    }
+    // C# Struct Example: Using Constructor and Method
+    public struct Rectanglle
+    {
+        public int width, height;
+
+        public Rectanglle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public void areaOfRectangle()
+        {
+            Console.WriteLine("Area of Rectangle is: " + (width * height));
+        }
+    }
+    public class TestStructss
+    {
+        public static void Main()
+        {
+            Rectanglle r = new Rectanglle(5, 6);
+            r.areaOfRectangle();
+        }
+
+    }
+}
